e578c50a96d7e23d4d1fdc4bdf4d81807fb4bf69
Jiazheng <shen.jia.zheng@gmail.com>
2024-07-02 14:09:00 +0800

    Add RTP header

--- Module/protocol.h
+++ Module/protocol.h
@@ -63,3 +63,32 @@ struct sniff_udp {
     unsigned short uh_sum;   /* udp checksum */
 };
 #define SIZE_UDP 8
+
+struct sniff_rtp {
+#if RTP_BIG_ENDIAN
+    unsigned int version:2;	/* protocol version */
+    unsigned int p:1;	/* padding flag */
+    unsigned int x:1;	/* header extension flag */
+    unsigned int cc:4;	/* CSRC count */
+    unsigned int m:1;	/* marker bit */
+    unsigned int pt:7;	/* payload type */
+#else
+    unsigned int cc:4;	/* CSRC count */
+    unsigned int x:1;	/* header extension flag */
+    unsigned int p:1;	/* padding flag */
+    unsigned int version:2;	/* protocol version */
+    unsigned int pt:7;	/* payload type */
+    unsigned int m:1;	/* marker bit */
+#endif
+    unsigned int seq:16;	/* sequence number */
+    uint32_t ts;		/* timestamp */
+    uint32_t ssrc;		/* synchronization source */
+    uint32_t csrc[1];	/* optional CSRC list */
+};
+
+/* RTP Header Extension */
+struct rtp_hdr_ext {
+    uint16_t ext_type;	/* defined by profile */
+    uint16_t len;		/* extension length in 32-bit word */
+};
+
